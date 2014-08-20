4934bcfc3b95f6423a24d12e59c65791c9b7c476
charlieroberts <charlie@charlie-roberts.com>
2014-08-20 00:15:19 -0700

    adding initial files

---
+++ scripts/gibber/audio/envelopes.js
@@ -0,0 +1,60 @@
+!function() {
+  "use strict"
+  
+  var Envelopes = {},
+      Gibberish = require('../../external/gibberish.2.0.min'),
+      Clock = require('../clock'),
+      curves = require('../mappings').outputCurves,
+      LINEAR = curves.LINEAR,
+      LOGARITHMIC = curves.LOGARITHMIC,
+      types = [
+        'Line',    
+      ],
+      _mappingProperties = {
+        Line: {
+          start: {
+            min: 0, max: 1,
+            output: LINEAR,
+            timescale: 'audio',
+          },
+          end: {
+            min: 0, max: 1,
+            output: LINEAR,
+            timescale: 'audio',
+          },
+          time: {
+            min: 0, max: 8,
+            output: LINEAR,
+            timescale: 'audio',
+          }
+        },
+      };
+  
+  for( var i = 0; i < types.length; i++ ) {
+  
+    (function() {
+      var type = Array.isArray( types[ i ] ) ? types[ i ][ 0 ] : types[ i ],
+          name = Array.isArray( types[ i ] ) ? types[ i ][ 1 ] : types[ i ]
+     
+      Envelopes[ name ] = function() {
+        var args = Array.prototype.slice.call(arguments, 0),
+            obj
+            
+        obj = new Gibberish[ type ]( args[0], args[1], Clock.time( args[2] ), args[3] )
+        //obj.type = 'Env'
+        obj.name = name
+      
+        $.extend( true, obj, Gibber.ugen )
+        
+        Gibber.createProxyProperties( obj, _mappingProperties[ name ] ) 
+        
+        Gibber.processArguments2( obj, args, obj.name )
+        
+        console.log( name + ' is created.' )
+        return obj
+      }
+    })()
+  }
+  
+  module.exports = Envelopes
+}()
---
+++ scripts/gibber/environment.js
@@ -0,0 +1,523 @@
+( function() {
+
+"use strict"
+
+var GE = Gibber.Environment = {
+  // REMEMBER TO CHECK WELCOME.INIT()
+  SERVER_URL : 'http://127.0.0.1:8080',
+  //SERVER_URL : 'http://gibber.mat.ucsb.edu',
+
+  init : function() { 
+    $script( ['external/codemirror/codemirror-compressed', 'external/interface', 'gibber/layout', 'gibber/notation'], 'codemirror',function() {
+      $script( ['gibber/mappings',
+                'gibber/gibber_interface',
+                'gibber/console',
+                'gibber/mouse',
+                'gibber/column',
+                'external/mousetrap',
+                'gibber/chat',
+                'gibber/share',
+                'gibber/code_objects',
+                'gibber/docs',
+                'gibber/keymaps',
+                'gibber/browser',
+                'gibber/account',
+                'gibber/demos',
+                ], function() {
+
+        GE.Keymap.init()
+
+        $( '#layoutTable' ).attr( 'height', $( window ).height() )
+        $( '#contentCell' ).width( $( window ).width() )
+
+        Gibber.proxy( window )
+
+        if( !window.isInstrument ) {
+          GE.Layout.init( GE )
+          window.Layout = GE.Layout
+          GE.Account.init()
+          GE.Console.init()
+          //GE.Console.open()
+          GE.Welcome.init()
+          GE.Share.open()
+          //GE.Demos.open()
+          GE.Layout.createBoundariesForInitialLayout()
+        }
+        
+        window.Notation = Gibber.Environment.Notation
+        
+        $script( 'gibber/keys', function() { Keys.bind( 'ctrl+.', Gibber.clear.bind( Gibber ) ) } )
+      });
+    })
+
+    $script( ['external/color', 'external/injectCSS', 'gibber/theme'], 'theme', function() {
+      if( !window.isInstrument ) {
+        GE.Theme.init()
+        GE.Storage.init()
+        GE.Menu.init()
+      }
+    })
+    
+    $script( ['gibber/graphics/graphics',  'external/spinner.min'], function() {
+      Gibber.Graphics.load()
+    })
+  },
+  selectCurrentBlock: function( editor ) { // thanks to graham wakefield
+      var pos = editor.getCursor();
+      var startline = pos.line;
+      var endline = pos.line;
+      
+      while ( startline > 0 && editor.getLine( startline ) !== "" ) {
+        startline--;
+      }
+      while( endline < editor.lineCount() && editor.getLine( endline ) !== "" ) {
+        endline++;
+      }
+      
+      var pos1 = {
+          line: startline,
+          ch: 0
+      }
+      var pos2 = {
+          line: endline,
+          ch: 0
+      }
+      var str = editor.getRange( pos1, pos2 )
+
+      return {
+          start: pos1,
+          end: pos2,
+          text: str
+      }
+  },
+  
+  Storage : {
+    values : null,
+    init : function() {
+      Storage.prototype.setObject = function( key, value ) { this.setItem( key, JSON.stringify( value ) ); }
+      Storage.prototype.getObject = function( key ) { var value = this.getItem( key ); return value && JSON.parse( value ); }
+      
+      this.values = localStorage.getObject( "gibber2" )
+
+      if ( ! this.values ) {
+        this.values = {
+          layouts : {}
+        }
+      }
+    },
+    
+    save : function() {
+      localStorage.setObject( "gibber2", this.values );
+    },
+  },
+
+  Help : {
+    open : function() {
+      this.col = GE.Layout.addColumn({ header:'Help' })
+
+      this.col.bodyElement.remove()
+      
+      this.getIndex()
+    },
+    getIndex : function() {
+      $( '#docs' ).empty()
+      $.ajax({
+        url: GE.SERVER_URL + "/help",
+        dataType:'html'
+      })
+      .done( function( data ) {
+        var help = $( data )
+        $( GE.Help.col.element ).append( help )
+        GE.Help.col.bodyElement = help
+        GE.Layout.setColumnBodyHeight( GE.Help.col )
+      }) 
+    }, 
+  },
+  Credits : {
+    open : function() {
+      this.col = GE.Layout.addColumn({ header:'Credits' })
+
+      this.col.bodyElement.remove()
+      
+      this.getIndex()
+    },
+    getIndex : function() {
+      $( '#docs' ).empty()
+      $.ajax({
+        url: GE.SERVER_URL + "/credits",
+        dataType:'html'
+      })
+      .done( function( data ) {
+        var credits = $( data )
+        $( GE.Credits.col.element ).append( credits )
+        GE.Credits.col.bodyElement = credits
+        GE.Layout.setColumnBodyHeight( GE.Credits.col )
+      }) 
+    }, 
+  },
+	
+	getSelectionCodeColumn : function( cm, findBlock ) {
+		var pos = cm.getCursor(), 
+				text = null,
+			  column = GE.Layout.fullScreenColumn === null ? GE.Layout.columns[ GE.Layout.focusedColumn ] : GE.Layout.__fullScreenColumn__
+		
+    if( column.mode.indexOf('glsl') > -1 ) { // glsl always executes entire block
+      var lastLine = cm.getLine( cm.lineCount() - 1 )
+      pos ={ start:{ line:0, ch:0 }, end: { line:cm.lineCount() - 1, ch:lastLine.length - 1 } }
+      text = column.value
+    }else{
+  		if( !findBlock ) {
+  			text = cm.getDoc().getSelection()
+
+  	    if ( text === "") {
+  	      text = cm.getLine( pos.line )
+  	    }else{
+          pos = { start: cm.getCursor('start'), end: cm.getCursor('end') }
+  	    	//pos = null
+  	    }
+  		}else{
+        var startline = pos.line, 
+  					endline = pos.line,
+  					pos1, pos2, sel
+      
+        while ( startline > 0 && cm.getLine( startline ) !== "" ) { startline-- }
+        while ( endline < cm.lineCount() && cm.getLine( endline ) !== "" ) { endline++ }
+      
+        pos1 = { line: startline, ch: 0 }
+        pos2 = { line: endline, ch: 0 }
+			
+        text = cm.getRange( pos1, pos2 )
+
+        pos = { start: pos1, end: pos2 }
+  		}
+    }
+		
+    GE.Keymap.flash(cm, pos)
+		
+		return { selection: pos, code: text, column:column }
+	},
+  
+	//TODO : this should probably be moved to the Gibber object at some point as it's not environment specific
+	modes : {
+		nameMappings : {
+			'javascript' : 'javascript',
+			'glsl-fragment' : 'x-shader/x-fragment',
+			'glsl-vertex'   : 'x-shader/x-vertex'      
+		},
+		javascript : {
+			run : function( column, value, position, codemirror, shouldDelay ) {
+				if( shouldDelay ) {
+					Gibber.Clock.codeToExecute.push({ code:value, pos:position, cm:codemirror })
+				}else{
+					Gibber.run( value, position, codemirror ) 
+				}
+			},
+			default: [
+	      "/*To execute code, select it and hit Ctrl+Enter.",
+        "* Ctrl+. stops audio. Press the help button for",
+        "* more keystrokes to use in Gibber.",
+        "*",
+	      "* Giblet #1 - by thecharlie",
+	      "* In this sketch, the mouse position drives the",
+	      "* pitch of drums, the carrier to modulation",
+	      "* ratio of FM synthesis, and the feedback and",
+	      "* time of a delay.",
+	      "*/",
+	      "",
+	      "a = Drums('x*o*x*o-')",
+	      "a.pitch = Mouse.Y",
+	      "",
+	      "b = FM({ ",
+	      "  attack:  ms(1),",
+	      "  index:   a.Out,",
+	      "  cmRatio: Mouse.X",
+	      "})",
+	      "",
+	      "b.fx.add(",
+	      "  Delay({",
+	      "    time:     Mouse.X,",
+	      "    feedback: Mouse.Y",
+	      "  })",
+	      ")",
+	      "",
+	      "b.play( ",
+	      "  ['c2','c2','c2','c3','c4'].random(),",
+	      "  [1/4,1/8,1/16].random(1/16,2) ",
+	      ")"
+	    ].join('\n'),
+		},
+		'glsl-fragment' : { 
+			run: function( column, value, position, codemirror, shouldDelay ) {        
+        value = Gibber.Graphics.PostProcessing.defs + value
+        
+	    	column.shader.material = new THREE.ShaderMaterial({
+	    		uniforms: column.shader.uniforms,
+	    		vertexShader: column.shader.vertexText || Gibber.Graphics.Shaders.defaultVertex,
+	    		fragmentShader: value
+	    	});
+			},
+			default: [
+      "// to execute changes to shader, hit ctrl+enter",
+      "// or ctrl+shift+enter to execute at next measure",
+      "",
+      "// defaults to .05 but should be mapped",
+      "uniform float amp;",
+      "// time is updated automatically at 1/60 per frame",
+      "uniform float time;",
+      "",
+      "// texture passed to shader",
+      "uniform sampler2D tDiffuse;",
+      "// pixel coordinate from {0,1}",
+      "varying vec2 p;",
+      "",
+      "void main() {",
+      "  // normalize pixel coordinates to {-1,1}",
+      "  vec2 uv = 2. * p - 1.;",
+      "  float _out = 0.;",
+      "",
+      "  for( float i = 0.; i < 8.; i++ ){",
+      "    uv.x += sin( uv.y + time ) * amp;",
+      "    uv.x = abs( 1./uv.x ) * amp;    ",
+      "    _out += abs( uv.x ) * amp;    ",
+      "  }",
+      "",
+      "  gl_FragColor = vec4( 1.-_out );",
+      "}",
+			].join( '\n' ),
+		},
+    'glsl-vertex' : {
+			run: function( column, value, position, codemirror, shouldDelay ) {
+	      var shader = Gibber.Graphics.Shaders.make( column.shader.fragmentText, value )
+        column.shader.vertexText = value
+	    	column.shader.material = new THREE.ShaderMaterial({
+	    		uniforms: column.shader.uniforms,
+	    		vertexShader: value,
+	    		fragmentShader: column.shader.fragmentText || Gibber.Graphics.Shaders.defaultFragment
+	    	});
+			},
+      default : [
+        "// note:varying vec3 position provides vertex position by default",
+        "// as are projectionMatrix and modelViewMatrix",
+        "varying vec2 p;",
+        "uniform float amp;",
+        "uniform float time;",
+        "",
+        "float rand(vec2 co){",
+        "  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);",
+        "}",
+        "",
+        "void main() {",
+        "  p = uv; // pass to fragment shader",
+        "  vec3 _position = position.xyz;",
+        "  float amt = 10.;",
+        "",
+        "  // both position and amp are needed to (kindof) guarantee seeds",
+        "  // that will yield random values.",
+        "  _position.x += (-amt + rand(position.xy * amp) * amt * 2.) * amp;",
+        "  _position.y += (-amt + rand(position.yz * amp) * amt * 2.) * amp;  ",
+        "  _position.z += (-amt + rand(position.xz * amp) * amt * 2.) * amp;",
+        "",
+        "  gl_Position = ",
+        "    projectionMatrix * ",
+        "    modelViewMatrix * ",
+        "    vec4( _position, 1.0 );",
+        "}",
+    	].join("\n"),
+    }
+	},
+  Metronome : {
+    shouldDraw: true,
+    canvas: null,
+    ctx: null,
+    width: null,
+    height: null,
+    color: '#252525',
+    
+    draw: function( beat, beatsPerMeasure ) {
+      if( this.shouldDraw && this.ctx !== null ) {
+        var beatWidth = this.width / beatsPerMeasure,
+            beatPos = ( beat - 1 ) * beatWidth;
+      
+        this.ctx.clearRect( 0, 0, this.width, this.height )
+        this.ctx.fillRect(  beatPos, 0,  beatWidth, this.height )
+      }
+    },
+    
+    init: function() {
+      this.canvas = $( "#header canvas" )
+      this.ctx = this.canvas[0].getContext( '2d' )
+    
+      this.width = this.canvas.width()
+      this.height = this.canvas.height()      
+    
+      this.canvas.attr( 'width', this.width )
+      this.canvas.attr( 'height', this.height )
+    
+      this.ctx.fillStyle = this.color
+    
+      var color = this.color
+      Object.defineProperty( this, 'color', {
+        get: function() { return color; },
+        set: function(v) { color = v; this.ctx.fillStyle = color; }
+      })
+      
+      window.Metronome = this
+    },
+    
+    off : function() {
+      this.ctx.clearRect( 0, 0, this.width, this.height )
+      this.shouldDraw = false;
+    },
+    
+    on : function() { this.shouldDraw = true; },
+  },
+  
+  Message: {
+    post : function( msgText ) {
+      var msgDiv = $( '<div>' )
+      msgDiv.css({
+          position:'fixed',
+          display:'block',
+          width:450,
+          height:200,
+          left: $( "thead" ).width() / 2 - 225,
+          top: $( window ).height() / 2 - 100,
+          backgroundColor: 'rgba(0,0,0,.85)',
+          border:'1px solid #666',
+          padding:'.5em',
+          zIndex:1000
+        })
+        .addClass( 'message' )
+        .append( $( '<button>' )
+          .on('click', function(e) { $( msgDiv ).remove(); })
+          .html('&#10005;')
+        )
+      
+      msgDiv.append( $('<p>').text( msgText ).css({ marginTop:'.5em' }) )
+      
+      $( 'body' ).append( msgDiv )
+
+      return msgDiv // return so it can be removed if needed
+    },
+    postFlash : function( text, time, css ) {
+      var msgDiv = $( '<div>' )
+      .css({
+        position:'fixed',
+        display:'block',
+        width:450,
+        height:'3em',
+        left: $( "thead" ).width() / 2 - 225,
+        top: $( window ).height() / 2 - 100,
+        backgroundColor: 'rgba(0,0,0,.85)',
+        border:'1px solid #666',
+        padding:'.5em',
+        zIndex:1000
+      })
+      .addClass( 'message' )
+      
+      if( css ) msgDiv.css( css )
+        
+      msgDiv.append( $('<p>').text( text ) )
+    
+      $( 'body' ).append( msgDiv )
+      
+      time = time || 2000
+      
+      msgDiv.fadeOut( time, function() { msgDiv.remove() } )
+    },
+    postHTML : function( html ) {
+      var msgDiv = $( '<div>' )
+      msgDiv.css({
+          position:'fixed',
+          display:'block',
+          width:450,
+          height:200,
+          left: $( "thead" ).width() / 2 - 225,
+          top: $( window ).height() / 2 - 100,
+          backgroundColor: 'rgba(0,0,0,.85)',
+          border:'1px solid #666',
+          padding:'.5em',
+          zIndex:1000
+        })
+        .addClass( 'message' )
+        .append( $( '<button>' )
+          .on('click', function(e) { $( msgDiv ).remove(); })
+          .html('&#10005;')
+        )
+      
+      msgDiv.append( html  )
+      
+      $( 'body' ).append( msgDiv )
+      
+      return msgDiv
+    },
+  },
+  
+  Menu : {
+    init: function() {
+      $( '#publishButton' ).on( 'click', function(e) {
+        GE.Account.newPublicationForm()
+      })
+      $( '#browseButton' ).on( 'click', function(e) {
+        GE.Browser.open()
+      })
+      $( '#addCodeButton' ).on( 'click', function(e) {
+        GE.Layout.addColumn({ fullScreen:true, type:'code' })
+      })
+      $( '#consoleButton' ).on( 'click', function(e) {
+        GE.Console.open()
+      })
+      $( '#chatButton' ).on( 'click', function(e) {
+        GE.Chat.open()
+      })
+      $( '#helpButton' ).on( 'click', function(e) {
+        GE.Help.open()
+      })
+      $( '#creditsButton' ).on( 'click', function(e) {
+        console.log('credits!')
+        GE.Credits.open()
+      })
+      $( '#forumButton' ).on( 'click', function(e) {
+        GE.Forum.open()
+      })
+    }
+  },
+  
+  Forum : {
+    open : function() {
+      window.open( 'http://lurk.org/groups/gibber' )
+    },
+  },
+  
+  Spinner: {
+    current : null,
+    spin: function( target ) {
+      $( target ).spin('small', '#fff')
+      
+      return function() { $( target ).spin( false ) }
+    },
+    remove: function() {
+      this.current.stop()
+    }
+  },
+  Welcome : {
+    init : function() {
+      //var col = GE.Layout.addColumn({ type:'form', fullScreen:false, header:'Welcome' })
+      //col.bodyElement.remove()
+      //console.log( "GE.SERVER_URL", GE.SERVER_URL )
+      $.ajax({
+        url: GE.SERVER_URL + "/welcome",
+        dataType:'html'
+      })
+      .done( function( data ) {
+        var welcome = $( data )
+        GE.Console.div.append( welcome )
+        // $( col.element ).append( welcome )
+        // col.bodyElement = welcome
+        // GE.Layout.setColumnBodyHeight( col )
+      })
+    },
+  },
+}
+
+})()
