5cc837ef75babc7d5132ca8bdd0d59efd040b508
charlieroberts <charlie@charlie-roberts.com>
2014-08-21 14:41:27 -0700

    adding gulpfile and package.json

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+var browserify = require( 'browserify' ),
+    gulp = require( 'gulp' ),
+    source = require( 'vinyl-source-stream' ),
+    buffer = require( 'vinyl-buffer' ),
+    uglify = require( 'gulp-uglify' );
+    
+gulp.task( 'default', function(){
+  var out = browserify({
+    entries: [ './scripts/main.js' ]
+  })
+
+  out.bundle()
+    .pipe( source('gibber.lib.js') )
+    .pipe( buffer() )
+    .pipe( uglify() )
+    .pipe( gulp.dest('./build/') );
+  
+  return out
+});
