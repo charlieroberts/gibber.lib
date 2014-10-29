30dcbc626c26d92bbd18f306b66b4c2f71be7a22
charlieroberts <charlie@charlie-roberts.com>
2014-10-29 15:18:59 -0700

    fixes for graphics and other misc items

---
+++ scripts/gibber/gibber.lib.js
@@ -0,0 +1,10 @@
+!function() {
+
+var Gibber = require( 'gibber.core.lib' )
+Gibber.Audio = require( 'gibber.audio.lib' )( Gibber )
+Gibber.Graphics = require( 'gibber.graphics.lib' )( Gibber )
+Gibber.Interface = require( 'gibber.interface.lib' )( Gibber )
+
+module.exports = Gibber
+
+}()
